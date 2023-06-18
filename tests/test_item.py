386e1ccca1a2b091ead4fe35ca5394437532f014
Балахонцева Анастасия <ayamileto@gmail.com>
2023-06-18 05:07:56 +0300

    add tests to init func

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,26 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def testing_data():
+    item = Item("apple", 1.0, 10)
+    return item
+
+
+def test_init(testing_data):
+    """ Тест на проверку корректности инициализации экземпляра класса Item """
+    assert testing_data.name == "apple"
+    assert testing_data.price == 1.0
+    assert testing_data.quantity == 10
+
+
+def test_all_items(testing_data):
+    """ Тест на проверку добавления экземпляра класса Item в список all """
+    assert testing_data in Item.all
+
+
+def test_init_error():
+    """ Тест на проверку возникновения исключения при передаче некорректных типов данных в конструктор """
+    with pytest.raises(TypeError):
+        Item(123, "abc", 1.0)
