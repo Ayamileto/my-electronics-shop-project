5768720d77aa00b1b420e8ff6099705911984ace
Балахонцева Анастасия <ayamileto@gmail.com>
2023-07-05 04:28:42 +0300

    add check for an integer and non-negative value for number_of_sim

--- src/phone.py
+++ src/phone.py
@@ -3,9 +3,26 @@ from src.item import Item
 
 class Phone(Item):
     """ Класс товара категории Телефон """
-    def __init__(self, name: str, price: float, quantity: int, number_of_sim) -> None:
+    def __init__(self, name: str, price: float, quantity: int, number_of_sim: int) -> None:
         super().__init__(name, price, quantity)
-        self.number_of_sim = number_of_sim
+        self._number_of_sim = None  # Инициализируем атрибут без присваивания ему значения
+        self.number_of_sim = number_of_sim  # Вызываем сеттер для проверки и установки значения
+
+
+    @property
+    def number_of_sim(self):
+        return self._number_of_sim
+
+
+    @number_of_sim.setter
+    def number_of_sim(self, value):
+        """
+        Проверяет переданное значения атрибуту на неотрицательное целое число иначе возвращает ValueError
+        """
+        if not isinstance(value, int) or value <= 0:
+            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
+        self._number_of_sim = value
+
 
     def __repr__(self):
         """
