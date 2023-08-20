32da7e065428bb8e92a30684c21b651cbdc40e27
Балахонцева Анастасия <ayamileto@gmail.com>
2023-08-20 04:49:29 +0300

    add keyboard class

--- src/keyboard.py
+++ src/keyboard.py
@@ -7,11 +7,4 @@ class LanguageMixin:
 
 class KeyBoard(Item, LanguageMixin):
     """ Класс для товара 'клавиатура' """
-    def __init__(self, name, price, quantity):
-        """
-        Инициализация класса наследуется от класса Item и LanguageMixin
-        """
-        super().__init__(name, price, quantity)
-        LanguageMixin.__init__(self)
-
-
+    pass
