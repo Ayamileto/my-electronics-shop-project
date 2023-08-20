a2ae4de67d0a672292b4c33d06ad92fa1f74140d
Балахонцева Анастасия <ayamileto@gmail.com>
2023-08-20 16:58:20 +0300

    add mixin and import to main file

--- src/keyboard.py
+++ src/keyboard.py
@@ -2,7 +2,25 @@ from src.item import Item
 
 
 class LanguageMixin:
-    pass
+    """
+    Класс, добавляющий метод смены языка к другим классам
+    """
+    def __init__(self):
+        self._language = "EN"
+
+    @property
+    def language(self):
+        return self._language
+
+    def change_lang(self):
+        """
+        Метод для смены языка
+        """
+        if self._language == "EN":
+            self._language = "RU"
+        else:
+            self._language = "EN"
+        return self
 
 
 class KeyBoard(Item, LanguageMixin):
